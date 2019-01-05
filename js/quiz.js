4eceef1fe54ab505b31bbd58d6aaef3cd2e914b3
AaronBowdler <aaronbowdler@gmail.com>
2019-01-05 21:29:28 +0000

    added 15 questions to quiz out of the 20

--- js/quiz.js
+++ js/quiz.js
@@ -6,47 +6,224 @@ $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
       {
-        'q': 'What does CPU mean?',
+        'q': 'What is the most important part of the computer?',
         'options': [
+          'Screen',
+          'Internet',
+          'MotherBoard'
+        ],
+        'correctIndex': 2,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What does CPU stand for?',
+        'options': [
+          'Contrast Power Usage',
           'Central Processing Unit',
-          'Central Printing Unit',
-          'Central Pipe Unit'
+          'Computer Programing Utilities'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Correct! Well the other two are mainly for the whole of the computer.',
+        'incorrectResponse': 'Come on, it\'s not that hard!'
+      },
+      {
+        'q': 'What is another name for a video card?',
+        'options': [
+          'Graphics Card',
+          'Display Card',
+          'All of the above'
+        ],
+        'correctIndex': 2,
+        'correctResponse': 'You\'re a genius! This tricks so many people out.',
+        'incorrectResponse': 'Well you are kind of right, but you would only get 50% of a mark for that question.'
+      },
+      {
+        'q': 'What year was the hard drive introduced?',
+        'options': [
+          '1956',
+          '1965',
+          '1974'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct! You are a true historian for computers.',
+        'incorrectResponse': 'Wrong! Someone needs to take more history lessons.'
+      },
+      {
+        'q': 'What type of circuitry does the ram use?',
+        'options': [
+          'Multiplexing and Demultiplexing',
+          'Dulplex',
+          'Simplex'
         ],
         'correctIndex': 0,
         'correctResponse': 'Good job, that was obvious.',
-        'incorrectResponse': 'Well, if you don\'t include it, your quiz won\'t work'
+        'incorrectResponse': 'Well, at least you tried.'
       },
       {
-        'q': 'How do you use it?',
+        'q': 'What statement describes peripherals?',
         'options': [
-          'Include jQuery, that\'s it!',
-          'Include jQuery and the plugin javascript.',
-          'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
-        ],
-        'correctIndex': 2,
-        'correctResponse': 'Correct! Sounds more complicated than it really is.',
-        'incorrectResponse': 'Come on, it\'s not that easy!'
-      },
-      {
-        'q': 'The plugin can be configured to require a perfect score.',
-        'options': [
-          'True',
-          'False'
+          'An ancillary device used to put information into and get information out of the computer?',
+          'Files that you create on the computer and store on the hard drive',
+          'The paper and ink that you put into the printer'
         ],
         'correctIndex': 0,
-        'correctResponse': 'You\'re a genius! You just set allowIncorrect to true.',
-        'incorrectResponse': 'Why you have no faith!? Just set allowIncorrect to true.'
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
       },
       {
-        'q': 'How do you specify the questions and answers?',
+        'q': 'What type of machines used to help people in the early mechanical tools?',
         'options': [
-          'MySQL database',
-          'In the HTML',
-          'In the javascript configuration'
+          'Calculating Machines',
+          'Time Machines',
+          'Printing Machines'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'True or False “Most video cards are not limited to simple display output?',
+        'options': [
+          'True',
+          'False',
+          'Not Mentioned'
         ],
         'correctIndex': 2,
-        'correctResponse': 'Correct! Refer to the documentation for the structure.',
-        'incorrectResponse': 'Wrong! Do it in the javascript configuration. You might need to read the documentation.'
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'Who invented the Hard drive?',
+        'options': [
+          'IMB',
+          'CIA',
+          'BBC'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What does CSS stand for',
+        'options': [
+          'Central Storage System',
+          'Cascading Style Sheet',
+          'Campion Sport Session'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What is another name for hardware components of a computer?',
+        'options': [
+          'Hardware Components Duh!',
+          'Peripherals',
+          'Input and Output Devices'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': '',
+        'options': [
+          '',
+          '',
+          ''
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': '',
+        'options': [
+          '',
+          '',
+          ''
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': '',
+        'options': [
+          '',
+          '',
+          ''
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': '',
+        'options': [
+          '',
+          '',
+          ''
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': '',
+        'options': [
+          '',
+          '',
+          ''
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'True or False "A Graphics Card is an expansion card which generates a feed of output images to a display"',
+        'options': [
+          'True',
+          'False',
+          'Not Mentioned'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What does NAS stand for?',
+        'options': [
+          'Never Awake Storage',
+          'Network Attached Storage',
+          'Network Attached Script'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What does ROM stand for?',
+        'options': [
+          'Registered Oppointed Memory',
+          'Read Only Memory',
+          'Random Ordinary Memory'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
+      },
+      {
+        'q': 'What is SSD similar to?',
+        'options': [
+          'CD',
+          'Memory Stick',
+          'CSS'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Good job, that was obvious.',
+        'incorrectResponse': 'Well, if you don\'t include it, your computer won\'t work'
       }
     ]
   });
